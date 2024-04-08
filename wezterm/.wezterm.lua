afe6da2511523f375a8a4c599d685d630a7a3676
torusn <nicholastorus@gmail.com>
2024-04-08 10:03:56 -0400

    Update wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,30 +2,35 @@
 
 local wezterm = require 'wezterm'
 
-local M = {}
+local config = {}
 
-M.color_scheme = 'Dracula'
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
 
-M.window_background_opacity = 0.90
+config.color_scheme = 'Dracula'
 
-M.font = wezterm.font_with_fallback({ 'FiraCode Nerd Font', "JetBrains Mono" })
+config.window_background_opacity = 0.90
 
-M.window_padding = {
+config.font = wezterm.font_with_fallback({ 'FiraCode Nerd Font', "JetBrains Mono" })
+
+config.window_padding = {
   left = 3,
   right = 3,
   top = 3,
   bottom = 3,
 }
 
-M.enable_tab_bar = true
-M.use_fancy_tab_bar = true
-M.hide_tab_bar_if_only_one_tab = true
-M.window_close_confirmation = "NeverPrompt"
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.window_close_confirmation = "NeverPrompt"
 
-M.enable_scroll_bar = true
+config.enable_scroll_bar = true
 
-M.default_prog = { "wsl.exe" }
-M.window_decorations = "RESIZE"
+-- config.default_prog = { "wsl.exe" }
+config.default_domain = 'WSL:Ubuntu'
+config.window_decorations = "RESIZE"
 
 local mux = wezterm.mux
 local act = wezterm.action
@@ -35,20 +40,21 @@ wezterm.on('gui-startup', function ()
     window:gui_window():maximize()
 end)
 
-M.inactive_pane_hsb = {
+config.inactive_pane_hsb = {
     saturation = 0.8,
     brightness = 0.7,
 }
-M.use_dead_keys = false
-M.scrollback_lines = 5000
-M.adjust_window_size_when_changing_font_size = false
-M.hide_tab_bar_if_only_one_tab = true
+config.use_dead_keys = false
+config.scrollback_lines = 5000
+config.adjust_window_size_when_changing_font_size = false
+config.hide_tab_bar_if_only_one_tab = true
 
-M.window_frame = {
+config.window_frame = {
     font = wezterm.font { family = 'Noto Sans', weight = 'Regular' },
 }
 
-M.keys = {
+config.leader = { key = 'a', mods = 'CMD', timeout_milliseconds = 1000 }
+config.keys = {
   { key = 'f', mods = 'CMD', action = act.SplitVertical { domain = 'CurrentPaneDomain' }, },
   { key = 'd', mods = 'CMD', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
   { key = 'h', mods = 'CMD', action = act.ActivatePaneDirection 'Left', },
@@ -58,5 +64,4 @@ M.keys = {
   { key = 'x', mods = 'CMD', action = act.CloseCurrentPane{ confirm = false } },
 }
 
-
-return M;
+return config;
